246ef47eb7009ca14a877f38e8c311fe788869e0
samf526 <steven.a.felix@gmail.com>
2018-01-18 14:20:34 -0500

    updated flask app, retrained model

--- Project/application/flaskexample/views.py
+++ Project/application/flaskexample/views.py
@@ -1,64 +1,30 @@
 from flask import render_template, request
 from flaskexample import app
-from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
-import psycopg2
-from a_Model import ModelIt
+from predictions import predict_similar
+import gensim
 
-user = 'stevenfelix' #add your username here (same as previous postgreSQL)                      
-host = 'localhost'
-dbname = 'birth_db'
-db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
-con = None
-con = psycopg2.connect(database = dbname, user = user)
+path = "/Users/stevenfelix/Documents/DataScience_local/Insight/"
+file = 'model_full_30M_sg_200_5_2'
+model = gensim.models.word2vec.Word2Vec.load(path+file)
+
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template("index.html",
-       title = 'Home', user = { 'nickname': 'Miguel' },
-       )
-
-@app.route('/db')
-def birth_page():
-    sql_query = """                                                                       
-                SELECT * FROM birth_data_table WHERE delivery_method='Cesarean';          
-                """
-    query_results = pd.read_sql_query(sql_query,con)
-    births = ""
-    for i in range(0,10):
-        births += query_results.iloc[i]['birth_month']
-        births += "<br>"
-    return births
-
-@app.route('/db_fancy')
-def cesareans_page_fancy():
-    sql_query = """
-               SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean';
-                """
-    query_results=pd.read_sql_query(sql_query,con)
-    births = []
-    for i in range(0,query_results.shape[0]):
-        births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-    return render_template('cesareans.html',births=births)
-
 @app.route('/input')
-def cesareans_input():
+def query_input():
     return render_template("input.html")
 
 @app.route('/output')
-def cesareans_output():
-  #pull 'birth_month' from input field and store it
-  patient = request.args.get('birth_month')
-    #just select the Cesareans  from the birth dtabase for the month that the user inputs
-  query = "SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean' AND birth_month='%s'" % patient
+def query_output():
+  # pull 'query' from input field and store it
+  query = request.args.get('query')
+  # just select the Cesareans  from the birth dtabase for the month that the user inputs
   print(query)
-  query_results=pd.read_sql_query(query,con)
-  print(query_results)
-  births = []
-  for i in range(0,query_results.shape[0]):
-      births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-      the_result = ModelIt(patient,births)
-  return render_template("output.html", births = births, the_result = the_result)
+  suggestions=predict_similar(query, model)
+  print(suggestions)
+  #births = []
+  #for i in range(0,query_results.shape[0]):
+  #    births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
+  #    the_result = ModelIt(patient,births)
+  return render_template("output.html", suggestions=suggestions.to_html(), query=query)
     
---
+++ Project/application/predictions.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 15:42:56 2018
+
+@author: stevenfelix
+"""
+
+
+# Logging code taken from http://rare-technologies.com/word2vec-tutorial/
+# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+from nltk.tokenize import RegexpTokenizer # tokenizing
+from nltk.corpus import stopwords  # list of stop words
+from nltk.stem.wordnet import WordNetLemmatizer # lemmatizer
+import pandas as pd
+
+tokenizer = RegexpTokenizer(r'\w+') # tokens separated by white spice
+stops = set(stopwords.words('english')) # list of english stop words
+lemma = WordNetLemmatizer()
+
+def clean(title, rmv_stop_words=False):
+    tokens = tokenizer.tokenize(title.lower())     # tokenize
+    if rmv_stop_words:
+        tokens = [i for i in tokens if not i in stops] # remove stop words
+    normalized = [lemma.lemmatize(token) for token in tokens] # lemma
+    return normalized
+
+def predict_similar(query, model, rmv_stop_words=False):
+    l = []
+    q = clean(query, rmv_stop_words=rmv_stop_words)
+    
+    print('Original query: {}'.format(query))
+
+    for word in q:
+        missing = q[:]
+        ind = q.index(word)
+        missing.remove(word)
+        print('\n')
+        for syn in model.most_similar([word],topn=3):
+            full = missing[:]
+            full.insert(ind,syn[0])
+            l.append(' '.join(full))
+    if l is None:
+        return "function did not return results"
+    else:
+        return pd.DataFrame(l)
+    
