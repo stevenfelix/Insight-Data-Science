8762a1556cfb341cea83d4016157bbbd3a79a522
samf526 <steven.a.felix@gmail.com>
2018-02-05 19:43:05 -0500

    moved Search Suggester files to own repository

---
+++ chdir.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 27 17:23:29 2018
+
+@author: stevenfelix
+"""
+# %%
+import os
+os.chdir('/Users/stevenfelix/Documents/DataScience_local/Insight/Demo_App')
